c86fb739d8ae93c7175cca0d7591a1975ac66170
formation <formation@172.26.10.85>
2016-03-22 17:00:13 +0100

    Préparation PDO

--- src/lib/databases.php
+++ src/lib/databases.php
@@ -1,1 +1,8 @@
 <?php
+
+function getPDO(){
+    $dsn = "mysql: host=".DB_HOST.";dbname=".DB_NAME;
+    $pdo = new PDO($dsn, DB_USER, DB_PASS, array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ));
+}
