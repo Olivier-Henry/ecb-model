006b70eb71b65fa79e9a6028d9843f6642bd58c0
formation <formation@FORMATI-4LB51HH>
2016-03-30 17:01:29 +0200

    Ajout de l'autoload pour le DAO

--- web/index.php
+++ web/index.php
@@ -8,6 +8,17 @@ require ROOT_PATH.'/conf/config.php';
 require ROOT_PATH.'/lib/mvc.php';
 require ROOT_PATH.'/lib/databases.php';
 
+function autoloader($class){
+    $path = "../classes/DAO/$class.php";
+    if(file_exists($path)){
+        include_once $path;
+    }else{
+        throw new Exception("La classe $class n'a pas été trouvée'");
+    }
+}
+
+spl_autoload_register("autoloader");
+
 setlocale(LC_MONETARY, 'fr_FR');
 
 $controller = getController();
